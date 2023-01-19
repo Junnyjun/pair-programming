8e9ce86885ba1e4d07b369fa9397834a410f7d67
gunny <45007556+gunkim@users.noreply.github.com>
2023-01-19 23:51:40 +0900

    :white_check_mark: Player, Rabbit 테스트 케이스 작성 (#8)

---
+++ src/test/kotlin/io/github/babiesdev/PlayerTests.kt
@@ -0,0 +1,49 @@
+package io.github.babiesdev
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@DisplayName("Player는")
+class PlayerTests {
+    @ParameterizedTest
+    @ValueSource(ints = [1, 5])
+    fun `한번에 최대 10칸 이내로 왼쪽으로 움직일 수 있다`(moveCount: Int) {
+        val rabbit: Participant = Player(Position(10))
+
+        val movedRabbit = rabbit.leftMove(moveCount)
+
+        assertThat(movedRabbit.position).isEqualTo(Position(10 - moveCount))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 5])
+    fun `한번에 최대 10칸 이내로 오른쪽으로 움직일 수 있다`(moveCount: Int) {
+        val rabbit: Participant = Player(Position(0))
+
+        val movedRabbit = rabbit.rightMove(moveCount)
+
+        assertThat(movedRabbit.position).isEqualTo(Position(moveCount))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 5])
+    fun `한번에 10칸을 초과해서 오른쪽으로 이동할 경우 예외가 발생한다`(moveCount: Int) {
+        val rabbit: Participant = Player(Position(0))
+
+        assertThrows<IllegalArgumentException> { rabbit.leftMove(moveCount) }
+            .apply { assertThat(message).isEqualTo("한번에 10칸을 초과해서 이동할 수 없습니다. (현재: ${moveCount})") }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 5])
+    fun `한번에 10칸을 초과해서 왼쪽으로 이동할 경우 예외가 발생한다`(moveCount: Int) {
+        val rabbit: Participant = Player(Position(0))
+
+        assertThrows<IllegalArgumentException> { rabbit.rightMove(moveCount) }
+            .apply { assertThat(message).isEqualTo("한번에 10칸을 초과해서 이동할 수 없습니다. (현재: ${moveCount})") }
+    }
+}
