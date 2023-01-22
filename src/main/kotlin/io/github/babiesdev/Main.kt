00f6ed9d7cf0e1c50f85bf27c93b72bdcc7e5f63
JunnyJun <chbe5082@naver.com>
2023-01-22 17:43:05 +0900

    REFACTOR(adapter) : make context

--- src/main/kotlin/io/github/babiesdev/Main.kt
+++ src/main/kotlin/io/github/babiesdev/Main.kt
@@ -3,36 +3,4 @@ package io.github.babiesdev
 import java.util.*
 
 
-fun main() {
-    var flag = true
-    var firstPlayer = Participant.Player(Position(0))
-    val firstRabbit = Participant.Rabbit(Position(Random().nextInt(70)))
-
-    while (flag) {
-        readln()
-            .also { Regex("""(\d)([LR])""").find(it) ?: throw IllegalArgumentException("입력값이 올바르지 않습니다. (입력값: $it)") }
-            .let { it[0].digitToInt() to it[1] }
-            .let { (digit, side) ->
-                when (side) {
-                    'L' -> firstPlayer =firstPlayer.leftMove(digit)
-                    'R' -> firstPlayer =firstPlayer.rightMove(digit)
-                    else -> throw IllegalArgumentException("입력값이 올바르지 않습니다. (입력값: $side)")
-                }
-            }
-            .also { println("player :: ${firstPlayer.position.value}") }
-            .let {
-                when (Random().nextBoolean()) {
-                    true -> firstRabbit.leftMove(Random().nextInt(6))
-                    false -> firstRabbit.rightMove(Random().nextInt(6))
-                }.also {
-                    println("RABBIT :: ${it.position.value}")
-                    if (firstPlayer.position.value == it.position.value) {
-                        println("축하합니다! 당신이 이겼습니다.")
-                        flag = false
-                    }
-                }
-            }
-    }
-
-
-}
+fun main() = Runner.run()
---
+++ src/main/kotlin/io/github/babiesdev/Runner.kt
@@ -0,0 +1,40 @@
+package io.github.babiesdev
+
+import io.github.babiesdev.domain.DEFAULT_PLAYER
+import io.github.babiesdev.domain.DEFAULT_RABBIT
+import io.github.handler.Input
+import java.util.*
+
+class Runner {
+
+    companion object {
+        fun run() {
+            var flag = true
+            var firstPlayer = DEFAULT_PLAYER
+            val firstRabbit = DEFAULT_RABBIT
+
+            while (flag) {
+                val read: Input.InputData = Input.Console().read()
+                read.let { (digit, side) ->
+                        when (side) {
+                            'L' -> firstPlayer =firstPlayer.leftMove(digit)
+                            'R' -> firstPlayer =firstPlayer.rightMove(digit)
+                        }
+                    }
+                    .also { println("player :: ${firstPlayer.position.value}") }
+                    .let {
+                        when (Random().nextBoolean()) {
+                            true -> firstRabbit.leftMove(Random().nextInt(6))
+                            false -> firstRabbit.rightMove(Random().nextInt(6))
+                        }.also {
+                            println("RABBIT :: ${it.position.value}")
+                            if (firstPlayer.position.value == it.position.value) {
+                                println("축하합니다! 당신이 이겼습니다.")
+                                flag = false
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
