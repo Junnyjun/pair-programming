d18b4d61ef38214891c70c3bc24a472ac755d43b
JunnyJun <chbe5082@naver.com>
2023-01-22 17:42:52 +0900

    REFACTOR(adapter) : moved

--- src/test/kotlin/io/github/babiesdev/RabbitTests.kt
+++ src/test/kotlin/io/github/babiesdev/RabbitTests.kt
@@ -1,6 +1,8 @@
 package io.github.babiesdev
 
-import io.github.babiesdev.Participant.Rabbit
+import io.github.babiesdev.domain.Participant
+import io.github.babiesdev.domain.Participant.Rabbit
+import io.github.babiesdev.domain.Position
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
